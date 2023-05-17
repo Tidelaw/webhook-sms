c6fac9f5e98cab06cd3de6c60f97322b48ce138a
Tidelaw <116292851+Tidelaw@users.noreply.github.com>
2023-05-17 23:10:23 +0700

    test new twilio method

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,4 +1,4 @@
-
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 export default async function handler(req, res) {
 
@@ -9,24 +9,27 @@ export default async function handler(req, res) {
 
       console.log(webhook_data)
       console.log(webhook_data[0].description)
-
-      const client = require('twilio')(process.env.accountSid, process.env.authToken);
       
-      client.messages
-          .create({
-                      body: `${webhook_data[0].description}`,
-                      to: `+${process.env.number}`
-          })
-          .then(message => console.log(message.sid))
-          .done();
+      client.messages.create({
+        to: process.env.number,
+        from: process.env.sender,
+        body: `${webhook_data[0].description}`
+      }).then(function(message) {
+        res.status(200);
+      });
 
-      res.status(200)
+      // client.messages
+      //     .create({
+      //                 body: `${webhook_data[0].description}`,
+      //                 to: `+${process.env.number}`
+      //     })
+      //     .then(message => console.log(message.sid))
+      //     .done();
+
+      // res.status(200)
     };
 
   }
 
   catch (err) { console.log(err) }
 }
-
-
-
